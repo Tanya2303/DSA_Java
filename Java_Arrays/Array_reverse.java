0412ff5134a5ef2947c9b53e8ed7ab70e4943dfa
Tanya Raikwar <tanya21raikwar@gmail.com>
2025-07-17 14:50:58 +0530

    arrays_prob

---
+++ Java_Arrays/Array_reverse.java
@@ -0,0 +1,31 @@
+package Java_Arrays;
+
+public class Array_reverse { 
+    public static void reverse(int arr[]){
+        int first = 0;
+        int last = arr.length-1;
+
+        while(first <= last){
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+
+            first ++;
+            last--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+        System.out.println("the array is given as:");
+        for(int i = 0;i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(" ");
+        reverse(arr);
+        System.err.println("The reverse array is: ");
+        for(int i = 0;i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
--- Searching_algo/Binary_search.java
+++ Searching_algo/Binary_search.java
@@ -1,5 +1,31 @@
 package Searching_algo;
-
+import java.util.*;
 public class Binary_search {
+    public static int binary_search(int num[],int key){
+        int start = 0;
+        int end = num.length-1;
+        
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(num[mid] == key){
+                return mid;
+            }
+            if(num[mid] < key){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num[] = {1,2,3,4,5,6,7,8,9};
+        System.out.print("Enter the value of key: ");
+        int key = sc.nextInt();
+        int index = binary_search(num, key);
+        System.out.println("The key is at index : " + index);
+
+    }
 }
