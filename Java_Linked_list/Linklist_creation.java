b05465542b6ddbdb912302015330617e4fe3ef47
Tanya Raikwar <tanya21raikwar@gmail.com>
2025-08-18 19:16:39 +0530

    adding in ll

--- Java_Linked_list/Linklist_creation.java
+++ Java_Linked_list/Linklist_creation.java
@@ -34,21 +34,45 @@ class Linklist_creation{
     }
 
     public void printList() {
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
         Node temp = head;
         while(temp != null) {
-            System.out.print(temp.data + " ");
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
+    }
+    public void addMid(int data, int index){
+        Node newNode = new Node(data);
+        if (index == 0) {  // insert at head
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        int i = 0;
+        while(i < index - 1 && temp != null) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
     public static void main(String[] args) {
         Linklist_creation ll = new Linklist_creation();
+        ll.printList(); // Output: List is empty
         ll.addFritst(2);
         ll.addFritst(1);
         ll.addLast(3);
         ll.addLast(4);
         System.out.println("Linked List:");
         ll.printList(); // Output: 1 2 3 4
+        ll.addMid(5, 2);
+        System.out.println("\nAfter adding 5 at index 2:");
+        ll.printList(); // Output: 1 2 5 3 4
 
     }
 
