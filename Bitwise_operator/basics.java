97e1aa37e775fb8cc4db48b6538131b4e6b28aac
Tanya Raikwar <tanya21raikwar@gmail.com>
2025-09-22 15:20:45 +0530

    array notes updated

---
+++ Bitwise_operator/basics.java
@@ -0,0 +1,14 @@
+package Bitwise_operator;
+
+public class basics {
+    
+}
+
+
+// Bitwise operators are used to perform bit-level operations on binary numbers. They operate on individual bits of integer types (like int, long, etc.) and are commonly used in low-level programming, graphics programming, and performance optimization. Here are the basic bitwise operators in Java:
+// 1. AND (&): The AND operator takes two bits and performs a logical AND operation. The result is 1 if both bits are 1; otherwise, it is 0.
+// 2. OR (|): The OR operator takes two bits and performs a logical OR operation. The result is 1 if at least one of the bits is 1; otherwise, it is 0.
+// 3. XOR (^): The XOR operator takes two bits and performs a logical exclusive OR operation. The result is 1 if the bits are different; otherwise, it is 0.
+// 4. NOT (~): The NOT operator is a unary operator that flips the bits of its operand. It changes 1s to 0s and 0s to 1s.
+// 5. Left Shift (<<): The left shift operator shifts the bits of its operand to the left by a specified number of positions. It fills the rightmost bits with 0s.
+//  
